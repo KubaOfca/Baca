cbe5f353f3ef7bfd06da5fd7bd8ccdabc394773c
Jakub Baran <b.kuba.13@gmail.com>
2021-04-30 13:08:51 +0200

    Shuffling and Prefix - Done

--- Baca_Zadanie6/src/Source.java
+++ Baca_Zadanie6/src/Source.java
@@ -6,6 +6,7 @@ public class Source {
 
     public static Scanner scn = new Scanner(System.in);
     public static String[] songsNames;
+    public static String commonPrefix;
     public static void main(String[] args) {
         int numberOfSets = scn.nextInt();
         for (int i = 0; i < numberOfSets; i++) {
@@ -15,9 +16,10 @@ public class Source {
                             //znak jaki dostanie to znak nowej lini ktory konczy wykonanie tej funckji. Musimy wiec wczytac ten znak \n i potem
                             //ponownie powtorzyc wykonanie nextline aby poprawnie wczytac dane
             songsNames = scn.nextLine().split(" ");
-
+            commonPrefix = songsNames[0];
             shufflingAndFindingPrefix(0,numberOfSongs-1);
             displayStringArray(songsNames, numberOfSongs);
+            System.out.println(commonPrefix);
 
         }
     }
@@ -25,7 +27,7 @@ public class Source {
     public static void shufflingAndFindingPrefix( int start, int end) {
         int half = (int) Math.ceil((start + end + 1.0)/2.0); // 2
 
-        if(half % 2 != 0){
+        if(half % 2 != 0 && (start + end + 1) > 2){
             if((start + end + 1) % 2 == 0){
                 for (int i = half-1; i < end-1; i++){
                     swap(i, i+1);
@@ -40,13 +42,24 @@ public class Source {
             half = (int) Math.ceil((start + end + 1.0)/2.0);
         }
 
-        int half_half = (start+half) / 2;
-        int j = 0;
-        for (int i = half_half; i < half; i++, j++) {
-            swap(i, half + j);
+        if((start + end + 1) > 2){
+            int half_half = (start+half) / 2;
+            int j = 0;
+            for (int i = half_half; i < half; i++, j++) {
+                swap(i, half + j);
+            }
         }
 
         if((end - start) <= 3) {
+            for(int i = start; i < end+1; i++){
+                while (songsNames[i].indexOf(commonPrefix) != 0){
+                    commonPrefix = commonPrefix.substring(0, commonPrefix.length() - 1);
+                    if (commonPrefix.isEmpty()){
+                        commonPrefix = "";
+                        return;
+                    }
+                }
+            }
             return;
         }
 
@@ -69,3 +82,30 @@ public class Source {
         System.out.println();
     }
 }
+
+/*
+Testy input
+5
+1
+a1
+3
+a1 a2 b1
+5
+a1 a2 a3 b1 b2
+6
+a1 a2 a3 b1 b2 b3
+7
+a1 a2 a3 a4 b1 b2 b3
+
+Testy output
+a1
+a1
+a1 b1 a2
+
+a1 b1 a2 b2 a3
+
+a1 b1 a2 b2 a3 b3
+
+a1 b1 a2 b2 a3 b3 a4
+
+ */
